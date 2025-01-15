b027712b6dc09b9e25ab4b9ebbd3113ba2d731f1
Trust-Worthy <jondeveloper0@gmail.com>
2025-01-15 15:52:26 -0500

    Learned about scope for ownership. Learned that the proper memory is auto asked for via the from() func for strings.

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+//
+// Project Name: Ownership
+// File: ownership/main.rs
+// Author: Trust-Worthy
+// Description: This is a file to learn about how ownership works in rust
+
+
+
+/// Looking at the scope of variables in this function
+fn ex_uno() {
+    // when s comes into scope it is valid
+    // when s is out of scope it's not
+    let _s:&str = "hello";
+
+    // create a string literal using from function
+    let mut s2: String = String::from("hello"); // this is an example of that static method ting!
+    // * must put the mut keyword!!!
+    s2.push_str(", world!"); //  appends a literal to a String
+
+    // String can be mutated by examples like s on line 13 cannot be mutated becaues of how these types deal with memory.
+    // My hypothesis is that string literals are placed on the stack
+
+    println!("{s2}");
 }
+
+
+
+
+
+
+
+
+
+
+
+fn main() {
+    ex_uno();
+}
+
+
