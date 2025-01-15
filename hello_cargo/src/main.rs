ea9bd64a24811705bcf7ba929bb0fc7350700a5d
Trust-Worthy <jondeveloper0@gmail.com>
2025-01-15 09:58:35 -0500

    Learned how to compile a rust program using rustc. Learned how to create a project using cargo, learned how to compile using cargo build, learned how to check rust code using cargo check.

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/**
+ * Create new project using --> cargo new 
+ * Build a project using --> cargo build
+ * Build and run a project in one step using --> cargo run
+ * Build project without producing executable to check for errors --> cargo check 
+ * Cargo stores resulting executable in --> target/debug
+ * 
+ */
