db20eb4de73d66386d372e9481fe4497b14d33a8
Trust-Worthy <jondeveloper0@gmail.com>
2025-01-15 11:56:18 -0500

    Learned about functions, arguments, and parameters. I also figured out how to use cargo docs to generate html documentation!

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -28,6 +28,10 @@ fn main() {
     let x:i32 = five();
 
     println!("The value of x is: {x}");
+
+    let y:i32 = plus_one(5);
+    println!("The value of y is: {y}");
+
 }
 
 /// Func example with parameter
@@ -61,4 +65,18 @@ fn expression_ex() {
 /// Returns the number 5
 fn five() -> i32 {
     5
+}
+/// Returns one integer greater than passed in argument 
+/// 
+/// #### Arguments 
+/// 
+/// * 'x' - a 32 bit signed number
+/// 
+/// #### Return 
+/// 
+/// a 32 bit signed number
+fn plus_one(x:i32) -> i32 {
+    x + 1
+    // If i were to put a ; at the end I would get an error
+    // a semicolon would turn x + 1 into a statement when the function says that it will return an i32
 }
