337daad974058c06c773b9f347d08618c5fa6401
Trust-Worthy <jondeveloper0@gmail.com>
2025-01-16 08:22:26 -0500

    Working on sha1 from scratch

---
+++ sha-1/src/main.rs
@@ -0,0 +1,8 @@
+mod sha1;
+
+use sha1::Sha1; /// I want to use the Sha1 datatype from the sha1 module
+
+fn main() {
+    let hasher = Sha1::new();
+}
+
---
+++ sha-1/src/sha1.rs
@@ -0,0 +1,7 @@
+pub struct Sha1;
+
+impl Sha1 {
+    pub fn new() -> Self { // uppercase Self means return an instance of this datatype
+        Sha1{}
+    }
+}
