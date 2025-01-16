14cd693b5b9f09c0fa8c4234396afb04a9d23560
Trust-Worthy <jondeveloper0@gmail.com>
2025-01-16 09:10:45 -0500

    Changed the padding temporarily to analyze the bits

--- sha-1/src/main.rs
+++ sha-1/src/main.rs
@@ -4,6 +4,6 @@ use sha1::Sha1; /// I want to use the Sha1 datatype from the sha1 module
 
 fn main() {
     let hasher: Sha1 = Sha1::new(); // new returns an instance of sha
-    
+    hasher.hash("TESTING sdfasdfasdweqwertqwtutyuljhjvgsdfghjhafSDFGDHAERTERTQF");
 }
 
--- sha-1/src/sha1.rs
+++ sha-1/src/sha1.rs
@@ -20,15 +20,30 @@ impl Sha1 {
         // borrowing &self --> not taking ownership of it, simply reading from it
         let mut bytes:Vec<u8> = input.as_bytes().to_vec(); // vectors allow us to add on additional items without having to resize on our own
         
+        println!("ascii vals of bytes: {:?}",bytes);
         let original_bit_length: u64 = bytes.len() as u64 * 8; // 8 bits in a byte --> want to get the original input in bits0
+        
+        println!("original bit length of those bytes: {:?}",original_bit_length);
+        bytes.push(0x80); // delimeter between old and new data
+        // [bytes] 1000000 [padding] [bit length]                                     0x80 = 128 = 1000000 
 
+        println!(" delimeter of 0x80 added to those bytes: {:?}",bytes);
         while (bytes.len() * 8) % 512 != 448 { // we want to be 64 bits short of 512
             bytes.push(0);
         }
 
+        println!(" padding added to byte: {:?}",bytes);
         // borrowing &original_bit_length --> not taking ownership of it, simply reading from it
         bytes.extend_from_slice(&original_bit_length.to_be_bytes()); // bytes is a vector, then we add on to the end 
+        println!(" original bit length of {original_bit_length} added to the bytes: {:?}",bytes);
 
-        return  // we 'll be basically taking 512 bit chunks
+        return  bytes // we 'll be basically taking 512 bit chunks
+
+        // steps
+        // 1. break string down into vector of bytes
+        // 2. get bit length of string
+        // 3. add delimeter
+        // 3. loop through original bytes array and pad with zeros
+        // 5. add the original bit length on to the end
     }
 }
