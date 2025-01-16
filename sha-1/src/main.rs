e1f0964b743115fc25ec4120ffa930b61571f888
Trust-Worthy <jondeveloper0@gmail.com>
2025-01-16 11:01:48 -0500

    Done with the project! Now it's time to work on exploiting sha1.

--- sha-1/src/main.rs
+++ sha-1/src/main.rs
@@ -1,11 +1,19 @@
 mod sha1;
 
 use sha1::Sha1; /// I want to use the Sha1 datatype from the sha1 module
-
+use std::io;
 fn main() {
     let mut hasher: Sha1 = Sha1::new(); // new returns an instance of sha
-        let res: [u8;20] = hasher.hash("TESTING");
-        println!(" byte version {:?}",res);
+
+    let mut user_input: String = String::new();
+
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Failed to take user input");
+
+    
+    let res: [u8;20] = hasher.hash(&user_input);
+    println!(" byte version {:?}",res);
 
          // Convert to a hexadecimal string for human readability
     let hex_string = res.iter().map(|b| format!("{:02x}", b)).collect::<String>();
