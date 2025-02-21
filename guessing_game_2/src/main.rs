ee3539174986c25378a24e54e3075b96b4d91b3e
Trust-Worthy <jondeveloper0@gmail.com>
2025-02-20 21:11:55 -0500

    Working on finishing the guessing game for the second time.

--- guessing_game_2/src/main.rs
+++ guessing_game_2/src/main.rs
@@ -7,26 +7,37 @@ fn main() {
     println!("Welcome to the guessing game...");
     println!("You're going to enter a few numbers and we gon see how nice you are at guessing.");
 
-    let secret_num: u8 = rand::thread_rng().gen_range(1..=100);
+    let secret_num: u32 = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is {secret_num}!");
 
 
-    println!("Please enter your guess below:\n-->");
-
+    
     
 
     let mut guess: String = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line!");
     
+    io::stdin() // this is the way to receive user in
+    .read_line(&mut guess)
+    .expect("Failed to read line!");
+
     println!("You guessed: {}",guess);
+    let guess: u32  = guess.trim().parse() {
+
+    }
     
-    match guess.cmp(&secret_num) {
-        Ordering::Less => println!("Guess higher!"),
-        Ordering::Greater => println!("Guess lower"),
-        Ordering::Equal => println!("Bingo! You got it."),
+
+    loop {
+        println!("Please enter your guess below:\n-->");
+
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Guess higher!"),
+            Ordering::Greater => println!("Guess lower"),
+            Ordering::Equal => {
+                println!("Bingo! You got it.");
+                break;
+            } 
+        }
     }
 
 
