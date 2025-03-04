d3622da1bcc212421208552662f2f85dd04ad7e6
Trust-Worthy <jondeveloper0@gmail.com>
2025-03-04 17:05:37 -0500

    Simple tuple in rust.

--- variables_2/src/main.rs
+++ variables_2/src/main.rs
@@ -12,6 +12,16 @@ fn main() {
     }
 
     let x = 6;
-    println!("The value of x is: {x} ");
+    println!("The value of x is: {x} ");    
+
+    // Example of a tuple:
+    let tup: (i32,f64,u8) = (500,6.4,1); /// I think the type annotations for a tuple are cool
+    let (x,y,z) = tup;
+
+    println!("The value of y is {y}");
+
+
+
+
 
 }
