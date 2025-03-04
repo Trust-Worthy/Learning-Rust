87c2068b137d005d991d86710072d2799e3413ff
Trust-Worthy <jondeveloper0@gmail.com>
2025-03-04 17:19:27 -0500

    Learning about indexing.

--- variables_2/src/main.rs
+++ variables_2/src/main.rs
@@ -1,5 +1,8 @@
-fn main() {
-    
+
+
+use std::io;
+
+fn initial_exercises() {
 
     let x:u8 = 5;
 
@@ -15,13 +18,61 @@ fn main() {
     println!("The value of x is: {x} ");    
 
     // Example of a tuple:
-    let tup: (i32,f64,u8) = (500,6.4,1); /// I think the type annotations for a tuple are cool
-    let (x,y,z) = tup;
+    let tup: (i32,f64,u8) = (500,6.4,1); // I think the type annotations for a tuple are cool
+    let (_x,_y,_z) = tup;
 
-    println!("The value of y is {y}");
+    println!("The value of y is {_y}");
+    println!("The value of x is {_x}");
+    println!("The value of y is {_y}");
 
 
+    // Arrays in rust have fixed sizes
+    let array: [i32; 5] = [4,5,6,7,8];
 
+    let first: i32 = array[0];
+    let second: i32 = array[1];
 
-
+    println!("First is {first}");
+    println!("Second is {second}");
 }
+
+
+
+
+
+fn get_array_index() {
+
+
+    let a: [u32;5] = [1,2,3,4,5];
+    println!("Please enter an array index.");
+
+    let mut index: String = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element: u32 = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
+}
+
+
+
+
+fn main() { 
+    initial_exercises();
+    get_array_index();
+    
+
+
+
+
+    }
+
+
