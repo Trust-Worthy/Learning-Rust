eb20a06e268ea9a2c5e913a02ecc1e522f33c35c
Trust-Worthy <jondeveloper0@gmail.com>
2025-03-06 17:07:38 -0500

    Processed the cmd line arguments into variables.

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -9,5 +9,11 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    dbg!(args);
+    let query: &String = &args[1]; // String to find in the file
+    let file_path: &String = &args[2]; // path to the file to be searched.
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
+
+    
 }
