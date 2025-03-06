3e1e157d98fe7b290b18bf059028035e005cb422
Trust-Worthy <jondeveloper0@gmail.com>
2025-03-06 17:34:16 -0500

    Learned that it's best to create a struct when two variabales are closely related. This helps keep the code maintainable and testable! I'm learning a lot of good software engineering principles.

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,13 +5,19 @@ use std::fs;
 
 
 
+struct  Config {
+    query: String,
+    file_path: String,
+}
 
-fn parse_configs(args: &[String]) -> (&str,&str) {
 
-    let query: &String = &args[1]; // String to find in the file
-    let file_path: &String = &args[2]; // path to the file to be searched.
+fn parse_configs(args: &[String]) -> Config  {
+    
 
-    return (query,file_path)
+    let query:String = args[1].clone(); // String to find in the file
+    let file_path:String = args[2].clone(); // path to the file to be searched.
+
+    return Config {query, file_path}
 }
 
 
@@ -24,14 +30,14 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    let (query,file_path) = parse_configs(&args);
+    let config: Config = parse_configs(&args);
 
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    println!("Searching for {}",config.query);
+    println!("In file {}",config.file_path);
 
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file.");
 
     println!("With text: \n{contents}");
