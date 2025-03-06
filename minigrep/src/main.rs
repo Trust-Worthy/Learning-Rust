3177eb177d4adb9c42fc9018b14e34b7de863fbd
Trust-Worthy <jondeveloper0@gmail.com>
2025-03-06 17:23:06 -0500

    Practing good rust programming structure. Try and keep as much out of the main func as possible. Put all logic in lib.rs file.

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,19 @@
 use std::env;
+use std::fs;
+
+
+
+
+
+
+fn parse_configs(args: &[String]) -> (&str,&str) {
+
+    let query: &String = &args[1]; // String to find in the file
+    let file_path: &String = &args[2]; // path to the file to be searched.
+
+    return (query,file_path)
+}
+
 
 
 
@@ -9,11 +24,17 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    let query: &String = &args[1]; // String to find in the file
-    let file_path: &String = &args[2]; // path to the file to be searched.
+    let (query,file_path) = parse_configs(&args);
+
 
     println!("Searching for {query}");
     println!("In file {file_path}");
 
-    
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file.");
+
+    println!("With text: \n{contents}");
+
+
 }
