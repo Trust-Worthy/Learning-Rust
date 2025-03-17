023757b466a034ee5dce3d046ce41069a3c39d86
Trust-Worthy <jondeveloper0@gmail.com>
2025-03-17 14:54:14 -0400

    Learned about references to arrays of integers too. And how I can get a immutabale reference to an integer array and assert the contents.

--- slicin_slices/src/main.rs
+++ slicin_slices/src/main.rs
@@ -11,7 +11,7 @@ fn main() {
     write a function that takes a string of words separated by spaces and returns the first word it finds in that string. 
     If the function doesn’t find a space in the string, the whole string must be one word, so the entire string should be returned.
      */
-    let mut s: String = String::from("hello twin");
+    let s: String = String::from("hello twin");
     
     let word: &str = first_word(&s[..]); // passing in a reference 
 
@@ -30,7 +30,12 @@ fn main() {
     let hello: &str = &string_to_slice[0..5]; // This is exclusive --> slice comprises index 0 - 4.
     let twin: &str = &string_to_slice[6..10];
 
-    
+    let a: [i32;5] = [1,2,3,4,5];
+
+    let slice: &[i32] = &a[1..3];
+
+    assert_eq!(slice,&[2,3])
+
 }
 
 
