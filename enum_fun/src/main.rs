bdba410a1fcf6ad8899db84f01086ed9348a1822
Trust-Worthy <jondeveloper0@gmail.com>
2025-03-17 18:05:56 -0400

    I never realized how powerful enums truly were in rust. That can do a lot more than enums in C and python it seems.

---
+++ enum_fun/src/main.rs
@@ -0,0 +1,30 @@
+
+
+
+enum IpAddrKind {
+    V4(u8,u8,u8,u8),
+    V6(String),
+}
+
+fn main() {
+    // Enums give you a way of saying a value is one of a possible set of values
+
+
+    // let four: IpAddrKind = IpAddrKind::V4;
+    // let six : IpAddrKind = IpAddrKind::V6;
+    
+    // You can put actual data inside the instance of an enum
+    // I can attatch data to each variant instead of making a whol ip address class
+    
+    // let home = IpAddrKind::V4(String::from("127.0.0.1"));
+    // let loopback = IpAddrKind::V6(String::from("::1"));
+
+    // Even better way?
+    let home = IpAddrKind::V4(127,0,0,1);
+    let loopback = IpAddrKind::V6(String::from("::1"));
+
+
+
+
+
+}
