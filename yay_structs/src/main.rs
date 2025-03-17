87c247abfda7a1a14119df7594bf8ed79879cc89
Trust-Worthy <jondeveloper0@gmail.com>
2025-03-17 15:20:18 -0400

    working on a function that creates / builds a new user.

--- yay_structs/src/main.rs
+++ yay_structs/src/main.rs
@@ -7,18 +7,28 @@ struct User {
     sign_in_count: u64,
 }
 
+fn build_user(email:String, username:String) -> User {
 
+    User {
+        active:true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
 
 fn main() {
 
-    let user1 = User {
+    let mut user1 = User { // entire instance must be mutable. Specific fields can't just be mutable
         active: true,
         username: String::from("testuser123"),
         email: String::from("testemail@yupyup.com"),
         sign_in_count: 1,
     };
 
-    
+    user1.email = String::from("newemail@yupyup.com");
+
+
 
 
 }
