a27b073d915a053f5253bfe56b3ff62781edcce7
Trust-Worthy <jondeveloper0@gmail.com>
2025-03-17 16:59:19 -0400

    Reworked it for structs.

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -1,6 +1,9 @@
 
 
-
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
 fn main() {
     // will take the width and height of a rectangle specified in pixels and calculate the area of the rectangle
@@ -13,10 +16,34 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         area(width1, height1)
     );
+
+    // Refactoring with tuples
+    let rect1 = (30,50);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_tuple(rect1)
+    );
+
+    // Refactoring with structs
+    let struct_rect1 = Rectangle{width:30,height:50};
+
+    
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_struct(&struct_rect1)
+    );
+
 }
 
+fn area_struct(rectangle: &Rectangle) -> u32 {
+    rectangle.height * rectangle.width
+}
 
-
+fn area_tuple(dimensions: (u32,u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+ 
 fn area(width: u32, height:u32) -> u32 {
     width * height
 }
