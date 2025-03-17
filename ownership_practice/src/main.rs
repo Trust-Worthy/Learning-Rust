a10b10715e157efe3dce1297fd5a38e78590005b
Trust-Worthy <jondeveloper0@gmail.com>
2025-03-17 12:55:42 -0400

    Working through all of chapter 4 to get a deeper more complete understanding of ownership.

--- ownership_practice/src/main.rs
+++ ownership_practice/src/main.rs
@@ -1,26 +1,32 @@
 fn main() {
-    let s: &str = "hello"; // string literal
+    // let s: &str = "hello"; // string literal
 
-    println!("String literal {s}");
+    // println!("String literal {s}");
 
-    let mut heap_str: String = String::from("hello bro");
+    // let mut heap_str: String = String::from("hello bro");
 
-    heap_str.push_str("what are you up to?");
-
-
-
-    let s1: String = String::from("hiiiii");
-
-    let s2: String = s1;
-
-    // println!("s1 is out of scope ! {s1}");
-    println!("s2 is a move of s1! {s2}");
+    // heap_str.push_str("what are you up to?");
 
 
-    // how to create a deep copy!
 
-    let s3: String = s2.clone();
-    println!("s3 has been officially cloned! clone: {s3}");
+    // let s1: String = String::from("hiiiii");
+
+    // let s2: String = s1;
+
+    // // println!("s1 is out of scope ! {s1}");
+    // println!("s2 is a move of s1! {s2}");
+
+
+    // // how to create a deep copy!
+
+    // let s3: String = s2.clone();
+    // println!("s3 has been officially cloned! clone: {s3}");
+
+    
+    // Reviewing & Refreshing on Chapter 4 of the Rust book
+    let mut s: String = String::from("hello");
+
+    s.push_str(", world");
 
     
 
