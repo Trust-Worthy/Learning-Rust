381b28229f797fe099ab6eac8d67c491f9c80636
Trust-Worthy <jondeveloper0@gmail.com>
2025-03-18 11:28:52 -0400

    I can add a second variable inside of a match statement that automatically binds to the value of the Quarter's state.

--- match_control_flow/src/main.rs
+++ match_control_flow/src/main.rs
@@ -1,3 +1,40 @@
+
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    Colorado,
+}
+
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+
+
+
+
 fn main() {
-    println!("Hello, world!");
+    
+}
+
+
+fn value_in_cents(coin: Coin) -> u8 {
+    // with an if statement the condition has to evalue to a bool
+    // but with match it can be any type
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        }
+    }
+
 }
