2dfc370958f5be06a9439e7d585475aaab2f09d7
Trust-Worthy <jondeveloper0@gmail.com>
2025-03-19 17:45:01 -0400

    I have 3 implementations of the same code. The last example is really cool to see and still easy to understand.

--- error_me_this/src/main.rs
+++ error_me_this/src/main.rs
@@ -1,4 +1,5 @@
-use std::{fs::File, io::ErrorKind};
+use std::fs::File;
+use std::io::{self, Read};
 
 fn panic_fun() {
 
@@ -36,6 +37,51 @@ fn reading_file() {
 
 }
 
+
+fn read_username_from_file() -> Result<String, io::Error> {
+
+
+    let username_file_result = File::open("nope.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) { // reads the contents of the file into username!
+ 
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+
+    // propogating erros is so common that the ? operator is used to help out.
+
+
+
+}
+
+fn clean_read_username_from_file() -> Result<String, io::Error> {
+
+    // using the question mark to propogate errors if an error occurs!
+    let mut username_file = File::open("bruh.txt")?;
+
+    let mut username = String::new();
+
+    username_file.read_to_string(&mut username)?;
+
+    Ok(username)
+}
+
+fn super_clean_read_username_from_file() -> Result<String, io::Error> {
+
+    let mut username = String::new();
+
+    File::open("asdfas.txt")?.read_to_string(&mut username)?;
+
+    Ok(username)
+}
 
 fn main() {
     
