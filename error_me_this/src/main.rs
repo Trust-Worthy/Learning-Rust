d5bb39c2ddab750746721cdb46554036adfbc62e
Trust-Worthy <jondeveloper0@gmail.com>
2025-03-19 17:26:03 -0400

    About to learn how to propogate and error properly.

--- error_me_this/src/main.rs
+++ error_me_this/src/main.rs
@@ -9,14 +9,7 @@ fn panic_fun() {
     v[99]; // this will panic
 }
 
-
-fn main() {
-    
-    // There are two types of errors in this rusty world
-    // 1. Recoverable errors --> usually report the bug to the user and retry the operation
-    // 2. Unrecoverable errors --> stop / crash the program because it usually refers to a bug
-    // there are no exceptions in rust! instead Result<T, E> is used for recoverable
-    // and the macro panic! is used for those unrecoverable ones
+fn reading_file() {
 
 
     let greeting_file_result = File::open("hello.txt"); // return type here is a Result<T,E>
@@ -34,7 +27,29 @@ fn main() {
         },
     };
 
+
+    let better = File::open("huh.txt").unwrap(); // unwrap is exactly like the match but it panics automatically
+
+    let informed = File::open("sadfasd.txt").expect("Custom error message that unwrap doesn't provide");
+
+    // in PRODUCTION code .expect() >>>>> .unwrap() because of the custom error messages.
+
+}
+
+
+fn main() {
     
+    // There are two types of errors in this rusty world
+    // 1. Recoverable errors --> usually report the bug to the user and retry the operation
+    // 2. Unrecoverable errors --> stop / crash the program because it usually refers to a bug
+    // there are no exceptions in rust! instead Result<T, E> is used for recoverable
+    // and the macro panic! is used for those unrecoverable ones
+
+
+    
+
+    // propogating the error is when you pass the error to the code that's calling 
+
 
 
 
