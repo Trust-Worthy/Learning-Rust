f2c3ddbbf1b20ddb3f1c2acee321b939f878fd3f
Trust-Worthy <jondeveloper0@gmail.com>
2025-03-19 13:39:44 -0400

    Now I understand the use statement with different crates specified.

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -22,6 +22,11 @@ fn deliver_order(){}
 
 mod back_of_house {
 
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
     pub struct Breakfast {
         pub toast: String,
         seasonal_fruit: String,
@@ -48,19 +53,34 @@ mod back_of_house {
     fn cook_order(){}
 }
 
-use back_of_house::Breakfast;
-pub fn eat_at_restaurant() {
 
-    // absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
 
-    // relative path 
-    front_of_house::hosting::add_to_waitlist();
+mod customer {
 
-    let mut meal = Breakfast::summer("rye");
-
-    meal.toast = String::from("Wheat"); // customer changing their food option
+    use super::back_of_house::{Breakfast,Appetizer};
+    use crate::front_of_house::hosting;
     
-
-
+    pub fn eat_at_restaurant() {
+    
+        // absolute path
+        crate::front_of_house::hosting::add_to_waitlist();
+    
+        // relative path 
+        hosting::add_to_waitlist();
+    
+        let mut meal = Breakfast::summer("rye");
+    
+        meal.toast = String::from("Wheat"); // customer changing their food option
+        // beacuse toast field is public I can write and read this field
+    
+        let order1 = Appetizer::Salad;
+        let order2 = Appetizer::Soup;
+        
+        hosting::add_to_waitlist();
+    
+    
+    }
 }
+
+
+use std::collections::HashMap; // std crate, collections module, Hashmap struct... Beautiful idiomatic rust at its finest
