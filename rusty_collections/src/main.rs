b7b8e347c39d5b763ecd9a416b39e03aef4d2dba
Trust-Worthy <jondeveloper0@gmail.com>
2025-03-19 14:16:51 -0400

    Working with vectors formally for the first time.

---
+++ rusty_collections/src/main.rs
@@ -0,0 +1,20 @@
+
+
+
+fn main() {
+    
+    // Vectors can only store values of the same type
+
+    let mut v: Vec<i32> = Vec::new();
+
+    let macro_vec = vec![1,2,3];
+
+
+    v.push(1);
+    v.push(2);
+    v.push(3);
+    v.push(5);
+
+
+    
+}
