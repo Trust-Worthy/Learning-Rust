d903ca877b4db2fa38b626cd78b77f85eccf101a
Trust-Worthy <jondeveloper0@gmail.com>
2025-03-19 14:21:50 -0400

    used what I learned yesterday about match statements.

--- rusty_collections/src/main.rs
+++ rusty_collections/src/main.rs
@@ -16,5 +16,18 @@ fn main() {
     v.push(5);
 
 
-    
+    let third_ele: &i32 = &v[2]; // getting a reference to an i32 / 3rd element in the vector
+
+    println!("Third element in the vector is {third_ele}");
+
+    let third_ele: Option<&i32> = v.get(2); // getting the element at the 2nd index in a different way
+
+    match third_ele {
+        Some(ele) => println!("Third element in the vector is {ele}"),
+        None => println!("Third element in the vector is NUTHIN"),
+    }
+
+
+
+
 }
