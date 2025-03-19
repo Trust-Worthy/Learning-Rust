59f2694b6c00ac7d78713cfac973b7ba07da4f75
Trust-Worthy <jondeveloper0@gmail.com>
2025-03-19 14:11:38 -0400

    Learning about the different collections that are built into rust../

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,25 +1,11 @@
 
 
-mod front_of_house {
-    
-   pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
+mod front_of_house;
+pub use crate::front_of_house::hosting; // example of re-exporting so that other modules / code that use this module can have access to i
 
 fn deliver_order(){}
 
+use back_of_house::{Breakfast,Appetizer};
 mod back_of_house {
 
     pub enum Appetizer {
@@ -43,43 +29,44 @@ mod back_of_house {
         }
     }
 
+use std::collections::*; // example of using glob operator to bring in all public items into scope 
 
+fn fix_incorrect_order() {
+    cook_order();
+    super::deliver_order();
+}
 
-    fn fix_incorrect_order() {
-        cook_order();
-        super::deliver_order();
-    }
-
-    fn cook_order(){}
+fn cook_order(){}
 }
 
 
 
+pub fn eat_at_restaurant() {
+    
+    // absolute path
+
+    // relative path 
+    hosting::add_to_waitlist();
+
+    let mut meal = Breakfast::summer("rye");
+
+    meal.toast = String::from("Wheat"); // customer changing their food option
+    // beacuse toast field is public I can write and read this field
+
+    let order1 = Appetizer::Salad;
+    let order2 = Appetizer::Soup;
+    
+    hosting::add_to_waitlist();
+
+
+}
+
 mod customer {
 
     use super::back_of_house::{Breakfast,Appetizer};
-    use crate::front_of_house::hosting;
-    
-    pub fn eat_at_restaurant() {
-    
-        // absolute path
-        crate::front_of_house::hosting::add_to_waitlist();
-    
-        // relative path 
-        hosting::add_to_waitlist();
-    
-        let mut meal = Breakfast::summer("rye");
-    
-        meal.toast = String::from("Wheat"); // customer changing their food option
-        // beacuse toast field is public I can write and read this field
-    
-        let order1 = Appetizer::Salad;
-        let order2 = Appetizer::Soup;
-        
-        hosting::add_to_waitlist();
+   
     
     
-    }
 }
 
 
@@ -87,6 +74,8 @@ mod customer {
 // can use the as keyword or just use fmt::Result and io::Result respectively
 use std::fmt::Result as fmtResult;
 use std::io::Result as IoResult;
+
+
 
 
 
